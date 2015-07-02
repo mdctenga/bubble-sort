cd05bd74b0d382eb84feaeaeff05697d851c8d6f
mdctenga <mdctenga@gmail.com>
2015-07-01 20:40:14 -1000

    iterated and swapped through the array once

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 var unsortedArray = [4, 3, 2, 7, 10, 6];
 
+
 bubbleSort(unsortedArray);
 
 function bubbleSort (unsortedArray){
+  var swapped = false;
+
   console.log(unsortedArray, " is in the function");
 
-  for (var i = 0; i <= unsortedArray.length - 1; i += 1) {
-    console.log("unsortedArray element " + i);
-  };
+  do {
+    for (var i = 0; i <= unsortedArray.length - 1; i += 1) {
+      // console.log("unsortedArray element " + i);
+      var current = unsortedArray[i];
+      var next = unsortedArray[i + 1];
+      if (current > next) {
+        // console.log("Bigger");
+        var proxy = current;
+        current = next;
+        next = proxy;
+      }
+    }
+  } while (console.log('hello'));
 }
 
 
